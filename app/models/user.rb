4b84ae4ba7f89a9bb34aa89af9a10c975960069d
Zhe Kuan Lin <53500272+VincentLin0619@users.noreply.github.com>
2023-10-19 11:11:31 +0800

    修正密碼驗證的錯誤訊息噴了兩次，原因（bcrypt本身就包含了驗證，model不需要再另外做）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :nick_name, :email, :password, message: "can't be blank"
-  validates_uniqueness_of :nick_name, :email, message: "is already taken"
+  validates_presence_of :nick_name, :email
+  validates_uniqueness_of :nick_name, :email
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ViteRailsDemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.default_locale = "zh-TW"
 
     # Configuration for the application, engines, and railties goes here.
     #
